e75d0a0b1c44e0046db06f0552fb86fc0a1bc194
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2024-12-17 12:21:52 +0100

    Add: Post user form example, using a simple Typescript logic

---
+++ app/(authentication)/signup/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+import { CustomUser, CustomUserForm } from "@/app/_type/ICustomUser"
+import { ChangeEvent, FormEvent, useState } from "react"
+
+export default function SignUp() {
+  const [customUser, setCustomUser] = useState<CustomUserForm>({
+    username: "",
+    password: "",
+    repeatPassword: "",
+  })
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  async function handleOnSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault() // Prevent reload of page
+
+    setIsLoading(true)
+
+    // Exclude repeat password
+    const newUser: CustomUser = {
+      username: customUser.username,
+      password: customUser.password,
+    }
+
+    // POST
+    const result = await fetch("http://localhost:8080/api/v1/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(newUser), // TODO - Unsupported Mediatype
+    })
+
+    if (result.ok) {
+      const data = await result.text()
+      console.log(data)
+    } else {
+      const resultError = await result.json()
+      console.error(resultError)
+    }
+
+    setIsLoading(false)
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setCustomUser((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-slate-800">
+      <form
+        onSubmit={handleOnSubmit}
+        method="post"
+        className="flex flex-col max-w-sm gap-4"
+      >
+        {/* Username */}
+        <label htmlFor="username">Username</label>
+        <input
+          className="text-black"
+          type="text"
+          name="username"
+          value={customUser.username}
+          onChange={handleChange}
+        />
+
+        {/* Password */}
+        <label htmlFor="password">Password</label>
+        <input
+          className="text-black"
+          type="password"
+          name="password"
+          value={customUser.password}
+          onChange={handleChange}
+        />
+
+        {/* RepeatPassword */}
+        <label htmlFor="repeatPassword">Confirm Password</label>
+        <input
+          className="text-black"
+          type="password"
+          name="repeatPassword"
+          value={customUser.repeatPassword}
+          onChange={handleChange}
+        />
+
+        {/* FIELD ERRORS */}
+        <p></p>
+
+        <button
+          className="bg-blue-600 p-4 rounded-md hover:bg-blue-500"
+          type="submit"
+          disabled={isLoading}
+        >
+          Sign Up{" "}
+          {isLoading ? (
+            <span className="inline-block animate-spin">↻</span>
+          ) : (
+            ""
+          )}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+/** TODO
+ *
+ * Fetch - POST (JSON, BODY)
+ *    Function - Call within button (onclick)
+ *
+ */
